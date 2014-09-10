7e8e42a807ba01cd99f654e9d24524a271bf418f
Kelly Luce <kellyeluce@gmail.com>
2014-09-09 23:53:05 -0700

    coordinates on map for San Francisco, data input, added map location and name

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 L.mapbox.accessToken = 'pk.eyJ1Ijoia2x1Y2UiLCJhIjoiUGtZSHRqWSJ9.jualfg8qfIHQHuLsaKlOBw';
 var map = L.mapbox.map('map', 'examples.map-i86nkdio')
-    .setView([40, -74.50], 9);
+    .setView([37.775, -122.418333333333], 10);
+
+
+
+
+
 
 var filters = document.getElementById('filters');
 var checkboxes = document.getElementsByClassName('filter');
@@ -29,19 +34,59 @@ change();
 $(document).ready(function(){
   $('.filter').on('click', function() { // This event fires when a button is clicked
       var button = $(this).val();
+      var query = "bloody%20mary";
       $.ajax({ // ajax call starts
           url: 'https://api.foursquare.com/v2/venues/search', // JQuery loads yelp.com
-          data: 'near=San+Francisco&query=bicycle&intent=browse&radius=10000&limit=10&client_id=V1NRKMSBFKTLAHAFV10W0RI0OHB1XRCR3WQUQBQTQPTZI1OM&client_secret=0PWPU23OCQVRECKPBCQ4LEJTEPI4AO3SCTIRTPOL2SQ1A3OQ&v=20140909', // Send value of the clicked button
+          data: 'near=San+Francisco&query='+  query +'&intent=browse&radius=10000&limit=10&client_id=V1NRKMSBFKTLAHAFV10W0RI0OHB1XRCR3WQUQBQTQPTZI1OM&client_secret=0PWPU23OCQVRECKPBCQ4LEJTEPI4AO3SCTIRTPOL2SQ1A3OQ&v=20140909', // Send value of the clicked button
           dataType: 'jsonp', // Choosing a JSON datatype
           success: function(data) // Variable data contains the data we get from serverside
           {
+
+          		
+
                 console.log(data);
-                for (var venue in data.response.venues) {
-                    console.log(venue.name);
+                // for (var venue in data.response.venues) {
+                for(var i = 0; i < data.response.venues.length; i++) {
+                	var lat = 	data.response.venues[i].location.lat;
+                	var lng = 	data.response.venues[i].location.lng;
+                	var title = data.response.venues[i].name;
+                	var description = data.response.venues[i].location.address;
+                    console.log("{" + lat + "," + lng + "}");
+
+					L.mapbox.featureLayer({
+					    // this feature is in the GeoJSON format: see geojson.org
+					    // for the full specification
+					    type: 'Feature',
+					    geometry: {
+					        type: 'Point',
+					        // coordinates here are in longitude, latitude order because
+					        // x, y is the standard for GeoJSON and many formats
+					        coordinates: [
+					          lng,
+					          lat,
+					        ]
+					    },
+					    properties: {
+					        title: title,
+
+					        description: description,
+					        // one can customize markers by adding simplestyle properties
+					        // https://www.mapbox.com/foundations/an-open-platform/#simplestyle
+					        'marker-size': 'large',
+					        'marker-color': '#BE9A6B',
+					        'marker-symbol': 'cafe'
+
+					    }
+					}).addTo(map);
+
+
+
                 }
             }
         });
     });
 });
+
+
 
 
