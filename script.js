099bfc465437dcbac2af04d4b22932db6d8980c9
Kelly Luce <kellyeluce@gmail.com>
2014-09-10 19:48:06 -0700

    changed marker color

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(document).ready(function(){
       var button = $(this).val();
       var query = "bloody%20mary";
       $.ajax({ // ajax call starts
-          url: 'https://api.foursquare.com/v2/venues/search', // JQuery loads yelp.com
+          url: 'https://api.foursquare.com/v2/venues/search', // JQuery loads foursquare.com
           data: 'near=San+Francisco&query='+  query +'&intent=browse&radius=10000&limit=10&client_id=V1NRKMSBFKTLAHAFV10W0RI0OHB1XRCR3WQUQBQTQPTZI1OM&client_secret=0PWPU23OCQVRECKPBCQ4LEJTEPI4AO3SCTIRTPOL2SQ1A3OQ&v=20140909', // Send value of the clicked button
           dataType: 'jsonp', // Choosing a JSON datatype
           success: function(data) // Variable data contains the data we get from serverside
@@ -73,7 +73,7 @@ $(document).ready(function(){
 					        // one can customize markers by adding simplestyle properties
 					        // https://www.mapbox.com/foundations/an-open-platform/#simplestyle
 					        'marker-size': 'large',
-					        'marker-color': '#BE9A6B',
+					        'marker-color': '#e74c3c',
 					        'marker-symbol': 'cafe'
 
 					    }
